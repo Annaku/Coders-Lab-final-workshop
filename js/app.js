e4e823642e82f54f604786be70239d46bb7e439f
annaku <anna.kuchnio@gmail.com>
2016-08-25 11:24:51 +0200

    Adds changes of months in the calendar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
   console.log("DOM załadowany");
 
@@ -32,9 +31,7 @@ $(window).on("resize", function(){
   $dropDown.fadeToggle("fast");
 });
 
-
 /*Slider*/
-
   var $currentIndex = 0;
   $items = $("#slider_container").find(".slider");
   console.log($items);
@@ -56,7 +53,6 @@ var $autoSlide = setInterval(function(){
 }, 3000);
 
 /*Photo details with description on click*/
-
 var $photo = $(".photo");
 var $description = $(".description");
 
@@ -73,6 +69,91 @@ $photo.on("click", function(){
     }
   });
 });
+
+/* Availability calendar */
+var $months = $(".month");
+var $index = 0;
+var $month = $months.eq($index);
+var $next = $("#next");
+var $prev = $("#prev");
+var $day = $(".day");
+
+function monthChange() {
+  $months.hide();
+  $month.show();
+  $day.filter(":gt(30)").css("color", "#ece1d7");
+  $day.filter(":gt(34)").hide();
+
+  $next.on("click", function (){
+    $index ++;
+    if ($index>0 && $index<=$months.length-1){
+      $months.eq($index-1).hide();
+      $months.eq($index).show();
+      if ($index === 1) {
+        $day.text(function (val){return +val-2});
+        $day.filter(":lt(3), :gt(31)").css("color", "#ece1d7");
+        $day.filter(":gt(2):lt(30)").css("color", "#af9884");
+      //  $day.filter(":gt(32)").css("color", "#ece1d7");
+      };
+      if ($index === 2) {
+        $day.html(function (val){return +val-4});
+        $day.filter(":lt(5), :gt(32)").css("color", "#ece1d7");
+        $day.filter(":gt(4):lt(31)").css("color", "#af9884");
+        $day.filter(":lt(42)").show();
+      };
+      if ($index === 3) {
+        $day.html(function (val){return +val+0});
+        $day.filter(":lt(1), :gt(30)").css("color", "#ece1d7");
+        $day.filter(":gt(0):lt(29)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+      if ($index === 4) {
+        $day.html(function (val){return +val-2});
+        $day.filter(":lt(3), :gt(31)").css("color", "#ece1d7");
+        $day.filter(":gt(2):lt(31)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+    };
+  });
+  $prev.on("click", function(){
+    $index--;
+    if ($index>=0 && $index<$months.length){
+      $months.eq($index+1).hide();
+      $months.eq($index).show();
+      if ($index === 0) {
+        $day.html(function (val){return +val+1});
+        $day.filter(":gt(30)").css("color", "#ece1d7");
+        $day.filter(":lt(30)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+      if ($index === 1) {
+        $day.text(function (val){return +val-2});
+        $day.filter(":lt(3), :gt(31)").css("color", "#ece1d7");
+        $day.filter(":gt(2):lt(30)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+      if ($index === 2) {
+        $day.html(function (val){return +val-4});
+        $day.filter(":lt(5), :gt(32)").css("color", "#ece1d7");
+        $day.filter(":gt(4):lt(31)").css("color", "#af9884");
+        $day.filter(":lt(42)").show();
+      };
+      if ($index === 3) {
+        $day.html(function (val){return +val+0});
+        $day.filter(":lt(1), :gt(30)").css("color", "#ece1d7");
+        $day.filter(":gt(0):lt(29)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+      if ($index === 4) {
+        $day.html(function (val){return +val-2});
+        $day.filter(":lt(3), :gt(31)").css("color", "#ece1d7");
+        $day.filter(":gt(2):lt(31)").css("color", "#af9884");
+        $day.filter(":gt(34)").hide();
+      };
+    };
+  });
+};
+  monthChange();
 
 
 /* Contact Form */
